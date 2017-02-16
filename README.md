98b2451c9f54cd9d198260f0b1d0a72952691ea8
Jianhong.Yin <yin-jianhong@163.com>
2017-02-16 16:30:19 +0800

    release getOpt-2.0
    Update the optionList format, improve the options group process:
    set optionList {
      $group_description1 {
        ...
      }
      $group_description2 {
        ...
      }
      $group_description3 {
        ...
      }
    }
    
    see: https://github.com/tcler/getopt.tcl/blob/master/getOpt-2.0/example.tcl

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ A getopt implementation in tcl that compat with GNU getopt_long_only(3) or getop
 Copy the dir getOpt-$version to /usr/local/lib or other specified dir.
 
 # how to use
+https://github.com/tcler/getopt.tcl/blob/master/getOpt-2.0/example.tcl
 https://github.com/tcler/getopt.tcl/blob/master/getOpt-1.0/example.tcl
---
+++ getOpt-2.0/example.tcl
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Jianhong Yin <yin-jianhong@163.com>
+# -*- tcl -*-
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+lappend auto_path .
+lappend auto_path /usr/local/lib
+package require getOpt 1.0
+namespace import ::getOpt::*
+
+# global var
+array set Opt {}
+array set InvalidOpt {}
+set NotOptions [list]
+set ForwardOpt {}
+set OptionList {
+	"*Options:" {}
+
+	"  *Options kkk1:" {
+		f	{arg y	help {#Specify a test list file}}
+		listf	{arg y	help {#Same as -f}	link f}
+		cc	{arg m	help {#Notify additional e-mail address on job completion}}
+	}
+
+	"\n  *Options kkk2:" {
+		kcov	{arg n	help {#insert kcov task for do the kernel test coverage check}}
+		kdump	{arg o	help {#insert kdump task for get core dump file if panic happen}}
+	}
+
+	"\n  *Options kkk3:" {
+		debugi	{hide y}
+		debugii	{hide y}
+		h	{}
+		repo	{forward y arg m	help {Configure repo at <URL> in the kickstart for installation}}
+		recipe	{forward y arg n	help {Just generate recipeSet node, internal use for runtest -merge}}
+	}
+}
+
+# getUsage test
+puts "Usage: $argv0 \[options\]"
+getUsage $OptionList
+
+# _parse_ argument
+getOptions $OptionList $::argv Opt InvalidOpt NotOptions ForwardOpt
+parray Opt
+parray InvalidOpt
+puts "NotOptions: $NotOptions"
+puts "ForwardOpt: $ForwardOpt"
